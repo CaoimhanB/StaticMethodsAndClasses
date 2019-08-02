34a4c51d8e836d2289f05f5b5bb3da07655dea47
Caoimhan Bradley <caoimhan@alchemytechs.com>
2019-08-02 11:03:05 +0100

    Add project files.

---
+++ StaticMethodsAndClasses/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticMethodsAndClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            UsefulTools.SayHi("John");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StaticMethodsAndClasses/UsefulTools.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticMethodsAndClasses
+{
+       class UsefulTools
+    {
+
+        public static void SayHi(string name)
+        {
+            Console.WriteLine("Hello " + name);
+        }
+
+
+    }
+}
